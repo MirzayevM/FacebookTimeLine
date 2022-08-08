07e2cb911774c4646f1f42eee8892f4c6829b33e
Mirzabek <mirzabekmirzayev746@gmail.com>
2022-08-08 09:53:24 +0500

    Initial Commit

---
+++ FacebookTimeLine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FacebookTimeLine
+//
+//  Created by Mirzabek on 08/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FacebookTimeLine/FacebookTimeLineApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookTimeLineApp.swift
+//  FacebookTimeLine
+//
+//  Created by Mirzabek on 08/08/22.
+//
+
+import SwiftUI
+
+@main
+struct FacebookTimeLineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
